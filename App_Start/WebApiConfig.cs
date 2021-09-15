eb44391caacdac298d85eb2fe8327b3dc456d05b
zzhukanton <zzhukanton@gmail.com>
2021-09-16 01:02:33 +0300

    Добавьте файлы проекта.

---
+++ App_Start/WebApiConfig.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+using System.Web.Http.Cors;
+
+namespace ExpensesAPI
+{
+	public static class WebApiConfig
+	{
+		public static void Register(HttpConfiguration config)
+		{
+			// Конфигурация и службы веб-API
+			config.EnableCors(new EnableCorsAttribute("http://localhost:4200", "*", "*"));
+
+			// Маршруты веб-API
+			config.MapHttpAttributeRoutes();
+
+			config.Routes.MapHttpRoute(
+				name: "DefaultApi",
+				routeTemplate: "api/{controller}/{id}",
+				defaults: new { id = RouteParameter.Optional }
+			);
+		}
+	}
+}
---
+++ Controllers/EntriesController.cs
@@ -0,0 +1,31 @@
+﻿using ExpensesAPI.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Cors;
+
+namespace ExpensesAPI.Controllers
+{
+	[EnableCors("http://localhost:4200", "*", "*")]
+    public class EntriesController : ApiController
+    {
+        public IHttpActionResult GetEntries()
+		{
+			try
+			{
+				using (var context = new AppDbContext())
+				{
+					var entries = context.Entries.ToList();
+					return Ok(entries);
+				}
+			}
+			catch (Exception ex)
+			{
+				return BadRequest(ex.Message);
+			}
+		}
+    }
+}
